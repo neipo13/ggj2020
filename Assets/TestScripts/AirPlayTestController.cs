9b87fa0409c6663e8e5fb6974396c78be96c4142
jonathan culp <jonathanculp@gmail.com>
2020-02-02 11:00:38 -0500

    Added intro music

--- Assets/TestScripts/AirPlayTestController.cs
+++ Assets/TestScripts/AirPlayTestController.cs
@@ -55,45 +55,51 @@ public class AirPlayTestController : MonoBehaviour {
                     currentLine.Add (current);
                 }
             }
-            
-            //clear old objects
-            foreach(var obj in lineObjs)
+
+            RenderLineData(lines);
+        }
+    }
+
+    public void RenderLineData(List<List<ImageData>> lines)
+    {
+        //clear old objects
+        foreach (var obj in lineObjs)
+        {
+            Destroy(obj);
+        }
+        lineObjs.Clear();
+
+        for (int j = 0; j < lines.Count; j++)
+        {
+            var line = lines[j];
+
+            //create a gameobject
+            var obj = new GameObject("Line_" + j);
+            obj.transform.parent = LineRendererParent;
+
+            // z offset is because new lines need to render on top of old
+            obj.transform.localPosition = new Vector3(0, 0, -j * 0.1f);
+            obj.transform.localScale = Vector3.one;
+
+            //obj.transform.position = Camera.main.ScreenToWorldPoint(new Vector3(0f, 0f, 0f));
+            lineObjs.Add(obj);
+
+            //create a line renderer component
+            var lineRenderer = obj.AddComponent<LineRenderer>();
+            lineRenderer.material = new Material(Shader.Find("Sprites/Default"));
+            lineRenderer.SetWidth(0.05f, 0.05f);
+            Color color;
+            ColorUtility.TryParseHtmlString(line.First().color, out color);
+            lineRenderer.material.color = color;
+
+            //plot all the points on the line & set color/thickness
+            lineRenderer.positionCount = line.Count;
+            lineRenderer.useWorldSpace = false;
+            for (int k = 0; k < line.Count; k++)
             {
-                Destroy(obj);
+                var point = line[k];
+                lineRenderer.SetPosition(k, new Vector3(point.x / 100f, -point.y / 100f, 10f));
             }
-            lineObjs.Clear();
-
-            for (int j = 0; j < lines.Count; j++) {
-                var line = lines[j];
-
-                //create a gameobject
-                var obj = new GameObject ("Line_" + j);
-                obj.transform.parent = LineRendererParent;
-
-                // z offset is because new lines need to render on top of old
-                obj.transform.localPosition = new Vector3(0, 0, -j * 0.1f);
-                obj.transform.localScale = Vector3.one;
-
-                //obj.transform.position = Camera.main.ScreenToWorldPoint(new Vector3(0f, 0f, 0f));
-                lineObjs.Add(obj);
-
-                //create a line renderer component
-                var lineRenderer = obj.AddComponent<LineRenderer> ();
-                lineRenderer.material = new Material (Shader.Find ("Sprites/Default"));
-                lineRenderer.SetWidth(0.05f, 0.05f);
-                Color color;
-                ColorUtility.TryParseHtmlString (line.First ().color, out color);
-                lineRenderer.material.color = color;
-
-                //plot all the points on the line & set color/thickness
-                lineRenderer.positionCount = line.Count;
-                lineRenderer.useWorldSpace = false;
-                for (int k = 0; k < line.Count; k++) {
-                    var point = line[k];
-                    lineRenderer.SetPosition (k, new Vector3 (point.x / 100f, -point.y / 100f, 10f));
-                }
-            }
-
         }
     }
 }
